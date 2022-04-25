7a70af40af5f01adf462c2d18ca3c10d4663cf08
Swapnil Dave <swapnil.dave@motadata.com>
2022-04-25 10:28:29 +0530

    TwoSum problem not completed...
    New Problem:
    Add Two Numbers
    Level:= Medium

--- src/main/java/com/mindarray/easy/TwoSum.java
+++ src/main/java/com/mindarray/easy/TwoSum.java
@@ -1,8 +1,32 @@
 package com.mindarray.easy;
 
+import java.util.Arrays;
+
 public class TwoSum {
 
     public static void main(String[] args) {
 
+        System.out.println(Arrays.toString(calculate(new int[]{10, 9, 2, 3, 4, 8, -1}, 9)));
+    }
+
+    public static int[] calculate(int[] nums, int target) {
+
+        var result = new int[2];
+
+        /*The sum of all the nums in the array is going to be greater than the target
+         * -1+2+3+4+10+8+9 = 35
+         * -1,2,3,4,8,9 = 25
+         * 10 = 10*/
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] <= target / 2) {
+
+            } else {
+
+            }
+        }
+
+        return result;
     }
 }
---
+++ src/main/java/com/mindarray/medium/AddTwoNumbers.java
@@ -0,0 +1,19 @@
+package com.mindarray.medium;
+
+import com.mindarray.medium.utils.ListNode;
+
+public class AddTwoNumbers {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        var result = new ListNode();
+
+        return result;
+    }
+}
+
+
+
